e9a44a034154703a718149668ecf77a16d42afb5
A-BhanuPrakashReddy <bhanuprakashreddy740@gmail.com>
2026-06-27 21:15:52 +0530

    Connect frontend to Railway backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,25 +1,29 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: 'https://skillbridge-platform-production-5138.up.railway.app',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 })
 
 api.interceptors.response.use(
-  r => r,
-  err => {
-    if (err.response?.status === 401) {
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
       localStorage.removeItem('token')
       localStorage.removeItem('user')
       window.location.href = '/login'
     }
-    return Promise.reject(err)
+    return Promise.reject(error)
   }
 )
 
